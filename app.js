a214f0d0cd6ca4cda175122cb7fb0e1012a4d83c
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-01-08 15:35:19 -0800

    incorporated first js element into index

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+'use strict'
 // comment
 
 /*
@@ -6,3 +6,19 @@ also
 a
 comment
 */
+const flavor = prompt("What kind of sauce do you preffer your accompany your food?", "Sweet? Spicy? Acidic? or other?");
+
+const tasty = document.getElementById("flavor");
+
+if (flavor && flavor.trim() !== "") {
+    tasty.textContent = `You like your food to be ${flavor}`;
+} else {
+    tasty.textContent = "Try something new";
+}
+// if (confirm("Do you like flavor in your life?")) {
+//  document.write("Welcome to FlavorTown!");
+// } else {
+//  document.write("My condolences... ):");
+// // }
+// let name = prompt("what kind of sauce do you preffer to accompany your plate?:", "Sweet? Spicy? Acidic?");
+// document.write("You preffer ", name, " sauces with your food.");
