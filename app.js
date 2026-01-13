57347365ecb60307bac82707877ca24f3c6b9bbc
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-01-12 21:13:32 -0800

    cleaned up order of prompts

--- app.js
+++ app.js
@@ -4,23 +4,29 @@
 // This runs when the page has finished loading
 window.onload = function() {
 // show confirm dialog with a question
-let likesFlavor = confirm("Do you like flavor in your life?");
+    let likesFlavor = confirm("Do you like flavor in your life?");
 
-if (likesFlavor) {
-    // user clicked OK (Yes)
-    alert("Welcome to FlavorTown!");
-} else {
-    // user clicked Cancel (No)
-    alert("My condolences... ");
-}
+    if (likesFlavor) {
+        // user clicked OK (Yes)
+        alert("Welcome to FlavorTown!");
+    } else {
+        // user clicked Cancel (No)
+        alert("My condolences... )':");
+    }
+
+    // second prompt, intteractive question
+    const flavor = prompt("What kind of sauce do you preffer your accompany your food?", "Sweet? Spicy? Acidic? or other?");
+
+    const tasty = document.getElementById("flavor");
+
+    if (tasty) { // ensures the element exists
+        if (flavor && flavor.trim() !== "") {
+            tasty.textContent = `You like your food to be ${flavor}.`;
+        } else {
+            tasty.textContent = "Try something new!";
+        }
+    }
+
 };
 
-const flavor = prompt("What kind of sauce do you preffer your accompany your food?", "Sweet? Spicy? Acidic? or other?");
 
-const tasty = document.getElementById("flavor");
-
-if (flavor && flavor.trim() !== "") {
-    tasty.textContent = `You like your food to be ${flavor}`;
-} else {
-    tasty.textContent = "Try something new";
-}
