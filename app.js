b2e87eb3e996acf6529530d96dafc94fc18d901c
Antonio Garcia Pardo <garcia54.a@gmail.com>
2026-01-13 15:58:36 -0800

    added functions to js prompts

--- app.js
+++ app.js
@@ -2,31 +2,33 @@
 // comment
 
 // This runs when the page has finished loading
-window.onload = function() {
-// show confirm dialog with a question
-    let likesFlavor = confirm("Do you like flavor in your life?");
+    function entryQuestion() {
+            // show confirm dialog with a question
+        let likesFlavor = confirm("Do you like flavor in your life?");
 
-    if (likesFlavor) {
-        // user clicked OK (Yes)
-        alert("Welcome to FlavorTown!");
-    } else {
-        // user clicked Cancel (No)
-        alert("My condolences... )':");
-    }
-
-    // second prompt, intteractive question
-    const flavor = prompt("What kind of sauce do you preffer your accompany your food?", "Sweet? Spicy? Acidic? or other?");
-
-    const tasty = document.getElementById("flavor");
-
-    if (tasty) { // ensures the element exists
-        if (flavor && flavor.trim() !== "") {
-            tasty.textContent = `You like your food to be ${flavor}.`;
+        if (likesFlavor) {
+            // user clicked OK (Yes)
+            alert("Welcome to FlavorTown!");
         } else {
-            tasty.textContent = "Try something new!";
+            // user clicked Cancel (No)
+            alert("My condolences... )':");
         }
-    }
+    };
 
-};
+    function sauceFlavor() {
+ // second prompt, intteractive question
+        const flavor = prompt("What kind of sauce do you preffer your accompany your food?", "Sweet? Spicy? Acidic? or other?");
 
+        const tasty = document.getElementById("flavor");
 
+        if (tasty) { // ensures the element exists
+            if (flavor && flavor.trim() !== "") {
+                tasty.textContent = `You like your food to be ${flavor}.`;
+            } else {
+                tasty.textContent = "Try something new!";
+            }
+        }
+    };
+
+entryQuestion();
+sauceFlavor();
